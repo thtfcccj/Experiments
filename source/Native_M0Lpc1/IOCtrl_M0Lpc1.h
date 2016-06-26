424d5138e1d33956b3be2be7006c908c2a016dde
thtfcccj <thtfcccj@126.com>
2016-06-26 18:05:30 +0800

    修正上一git版本IOCtrl_M0Lpc1中的定义问题

--- source/Native_M0Lpc1/IOCtrl_M0Lpc1.h
+++ source/Native_M0Lpc1/IOCtrl_M0Lpc1.h
@@ -10,22 +10,18 @@
 #include "LPC12xxbit.h"
 
 //-------------------------对LIGHT指示灯口的所有操作--------------------------
-//对应硬件的LED1
-#define TRIS_LIGHT		TRISA
-#define PORT_LIGHT		LATA
-#define PIN_LIGHT		PORTA
-#define BIT_LIGHT		0x20
+//假定IO口为GPIO2_0
 //方向
 #define OutLIGHT()		do{LPC_GPIO2->DIR |= (1 << 0);}while(0)
 //输出时,高低电平
 #define SetLIGHT()		do{LPC_GPIO2->SET = (1 << 0);}while(0)
 #define ClrLIGHT()		do{LPC_GPIO2->CLR = (1 << 0);}while(0)
-//#define IsSetLIGHT()		()
+#define IsSetLIGHT()		(LPC_GPIO2->OUT & (1 << 0))
 //初始化及配置为指示灯不亮
 #define CfgLIGHT() do{LPC_IOCON->PIO2_0 = 0x90; SetLIGHT(); OutLIGHT();}while(0)
 
 //-------------------------对TEST功能口的操作--------------------------
-//对应硬件的
+//假定IO口为GPIO2_1
 //方向
 #define InTEST()		 do{LPC_GPIO2->DIR &= ~(1 << 1);}while(0)
 //输入时,是否为高电平
@@ -33,7 +29,7 @@
 //输入时,上接电阻(只有PORTB及部分口有)
 #define SetPullUpTEST()		do{}while(0)
 //初始化及配置为输入上拉状态
-#define CfgTEST() do{LPC_IOCON->PIO2_0 = 0x100; SetPullUpTEST(); InTEST();}while(0)
+#define CfgTEST() do{LPC_IOCON->PIO2_1 = 0x90; SetPullUpTEST(); InTEST();}while(0)
 
 //-------------------------IO口的统一配置操作--------------------------
 //此函数在开机初始化时统一调用,可用于置不会用到的IO口为需要的状态等
